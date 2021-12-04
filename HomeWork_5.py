cdc7ecb5dc7daa3590c83aa631342ec1b2bdbe6a
Soprano1604 <Soprano1604@gmail.com>
2021-12-04 16:24:18 +0200

    Homework_5

--- HomeWork_5.py
+++ HomeWork_5.py
@@ -1,1 +1,10 @@
 # Написать программу которая выводит простые числа до введённого пользователем.
+
+while True:
+    print('Please enter the number:')
+    y = input()
+    if y == 'exit':
+        break
+    for num in range(2, int(y)):
+        if all(num % x != 0 for x in range(2, num)):
+            print(num)
