0b9096af9ad33ef391b62443c9d3f4304625756d
Soprano1604 <Soprano1604@gmail.com>
2021-12-05 00:09:51 +0200

    .

--- homework_3.py
+++ homework_3.py
@@ -2,11 +2,3 @@
 # Приоритеты операторов не учитывать
 # Дополнительно и необязательно - учитывать приоритеты
 
-while True:
-    print('Please enter the number:')
-    y = input()
-    if y == 'exit':
-        break
-    for num in range(2, int(y)):
-        if all(num % x != 0 for x in range(2, num)):
-            print(num)
