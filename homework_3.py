58730ad61cf4afa9636fb835805631114d4b42ba
Soprano1604 <Soprano1604@gmail.com>
2021-12-06 19:45:44 +0200

    Билеты с остатками и процентами

--- homework_3.py
+++ homework_3.py
@@ -8,7 +8,7 @@ while current_element != '=':
     current_element = input()
     if current_element == '=':
         break
-operations = operations + [current_element]
+    operations = operations + [current_element]
 current_operator = ''
 result = 0
 for element in operations:
