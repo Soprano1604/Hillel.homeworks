ad90af6c36472d93eb6b0bdbe58ce8687d09ac7d
Soprano1604 <Soprano1604@gmail.com>
2021-12-19 00:43:50 +0200

    more metr

--- HomeWork_11.py
+++ HomeWork_11.py
@@ -1,4 +1,4 @@
-input_measure = [(5, 'sm'), (3, 'in'), (1, 'ft'), (2, 'sa')]
+input_measure = [(5, 'sm'), (3, 'in'), (1, 'ft'), (2, 'sa'), (0.5, 'sm')]
 
 '''
 1m = 3.28084 ft
@@ -7,21 +7,22 @@ input_measure = [(5, 'sm'), (3, 'in'), (1, 'ft'), (2, 'sa')]
 1m = 0.46869 sa
 '''
 
-def converter(length, measure):
+def converter(measure_object):
+    convert_type = measure_object[1]
+    length = measure_object[0]
     result = 0
-    if measure == 'sm':
+    if convert_type == 'sm':
         result = length * 100
-    elif measure == 'in':
+    elif convert_type == 'in':
         result = length * 39.3701
-    elif measure == 'ft':
+    elif convert_type == 'ft':
         result = length * 3.28084
-    elif measure == 'sa':
+    elif convert_type == 'sa':
         result = length * 0.46869
     else:
         result = 'Unknown'
     return result
 
-for item in input_measure:
-    if item[0] >= 1:
-        converted = converter(item[0], item[1])
-        print(converted)
+result = map(converter, filter(lambda itm_in: True if itm_in[0] >= 1 else False, input_measure))
+for itm in result:
+    print(itm)
